e43c1086eefa295a8246d0e8a347287004cfe735
Hemamali2 <145467406+Hemamali2@users.noreply.github.com>
2023-10-04 15:20:37 +0530

    Update README.py

--- README.m
+++ README.m
@@ -4,4 +4,4 @@ py- 👋 Hi, I’m @Hemamali2
 
 <!---
 Hemamali2/Hemamali2 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your cha
+You can click the Preview link to tke a look at your cha
